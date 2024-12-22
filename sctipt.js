f2d384c495590f9fe6032ad00b0311feaa670717
JemKeHU <staslovel1338@gmail.com>
2024-12-22 19:00:21 +0000

    Improved Etch-a-Scetch

--- sctipt.js
+++ sctipt.js
@@ -1,5 +1,20 @@
 const container = document.querySelector("#container");
 const blackColor = "black";
+const tools = document.querySelectorAll(".instruments button");
+let currentTool = "pen";
+
+function resetToolButtons() {
+    tools.forEach((button) => button.classList.remove('active'));
+}
+
+tools.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        resetToolButtons();
+        event.currentTarget.classList.add("active");
+        currentTool = event.currentTarget.id.replace("-tool", "");
+        console.log("Выбран инструмент:", currentTool);
+    });
+});
 
 for (let i = 1; i < 257; i++) {
     const div = document.createElement("div");
@@ -9,14 +24,18 @@ for (let i = 1; i < 257; i++) {
 
 container.addEventListener("mouseover", (event) => {
     if (event.target.classList.contains("normal")) {
-        event.target.classList.add(blackColor);
+        if (currentTool === "pen") {
+            event.target.style.backgroundColor = blackColor;
+        } else if (currentTool === "eraser") {
+            event.target.style.backgroundColor = "white";
+        }
     }
 });
 
 const resetBtn = document.querySelector(".reset");
 
 resetBtn.addEventListener("click", () => {
-    document.querySelectorAll(`#container > .${blackColor}`).forEach(n => {
-        n.classList.remove(blackColor);
+    document.querySelectorAll(`#container > .normal`).forEach(div => {
+        div.style.backgroundColor = "";
     });
 });
